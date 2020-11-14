11ef3fbd57bd58ac3a40c9fb2294789c4585fd09
P.Ackroyd <peter.r.ackroyd@gmail.com>
2020-11-14 08:42:28 +0000

    apparently i still need to find out the basics of git

--- client/calendar-collaborator/src/App.js
+++ client/calendar-collaborator/src/App.js
@@ -1,48 +1,123 @@
-import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
-import ApiCalendar from 'react-google-calendar-api';
-import googleapis from './googleapis.js';
+import LogIn from './components/logIn'
+import AddEvent from './components/addEvent'
+import React, {useState, useLayoutEffect, useEffect} from 'react';
+import apiCalendar from './ApiCalendar'
+import Timeline from 'react-calendar-timeline';
+import 'react-calendar-timeline/lib/Timeline.css';
+import moment from 'moment';
 
-class DoubleButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleItemClick = this.handleItemClick.bind(this);
-  }
-  
-  handleItemClick(event, name) {
+
+function App() {
+  const [eventList, setEventList] = useState([]);
+  const [ready, setReady] = useState(false);
+  const [groups, setGroups] = useState([{ id: 1, title: 'Global Events' }]);
+
+  let items = []
+  useEffect(() => {
+    if (!ready && eventList.length) {
+      let groupID = 2;
+      const groupTemp= [];
+      for (let i=0;i<eventList.length;i++) {
+        if (eventList[i].attendees) {
+          for (const attendee of eventList[i].attendees) {
+            let groupFind = groups.filter(group => group.title === attendee);
+            if (!groupFind.length) {
+              groupTemp.push({ id: groupID, title: attendee.email });
+              groupID++;
+            }
+          }
+        }
+      }
+      setGroups([{ id: 1, title: 'Global Events' }, ...groupTemp]);
+      setReady(true);
+    }
+  },[eventList]);
+
+  useEffect(() => {
+    let itemID = 0;
+    for (let i=0;i<eventList.length;i++) {
+      if (eventList[i].attendees) {
+        for (const attendee of eventList[i].attendees) {
+          let groupFind = groups.filter(group => group.title === attendee.email);
+          let group=1;
+          if (groupFind.length) {
+            group = groupFind[0].id;
+          }
+          items.push(
+            {
+              id: itemID,
+              group: group,
+              title: eventList[i].summary,
+              start_time: moment(eventList[i].start.dateTime),
+              end_time: moment(eventList[i].end.dateTime)
+            }
+          )
+          itemID++;
+        }
+      } else {
+        items.push(
+          {
+            id: itemID,
+            group: 1,
+            title: eventList[i].summary,
+            start_time: moment(eventList[i].start.dateTime),
+            end_time: moment(eventList[i].end.dateTime)
+          }
+        )
+        itemID++;
+      }
+    }
+    console.log(items)
+  });
+
+
+  const handleItemClick = async (event, name) => {
     if (name === 'sign-in') {
-      ApiCalendar.handleAuthClick();
+      if (apiCalendar.gapi) {
+        try {
+          await apiCalendar.gapi.auth2.getAuthInstance().signIn();
+          await apiCalendar.listEvents().then((res)=>setEventList(res.result.items));
+          apiCalendar.updateSignedIn(true);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+        else {
+        console.log("Error: this.gapi not loaded");
+      }
     } else if (name === 'sign-out') {
-      ApiCalendar.handleSignoutClick();
+      if (apiCalendar.gapi) {
+        const auth2 = apiCalendar.gapi.auth2.getAuthInstance();
+        await auth2.signOut().then(function () {
+          apiCalendar.updateSignedIn(false);
+          setEventList([]);
+        });
+      } else {
+        console.log("Error: gapi not loaded");
+      }
+      console.log(apiCalendar.signedIn)
+    } else if (name === 'list') {
+      console.log(eventList);
     }
   }
 
-  render() {
-    return (
-        <>
-        <h1>1234</h1>
-          <button
-              onClick={(e) => this.handleItemClick(e, 'sign-in')}
-          >
-            sign-in
-          </button>
-          <button
-              onClick={(e) => this.handleItemClick(e, 'sign-out')}
-          >
-            sign-out
-          </button>
-        </>
-      );
-  }
-}
-
-function App() {
   return (
-    <div className="App">
-      <googleapis/>
-      <DoubleButton/>
-    </div>
+    <>
+      {apiCalendar.signedIn ?
+      (ready ?<>
+        <Timeline
+          groups={groups}
+          items={items}
+          defaultTimeStart={moment().add(0, 'hour')}
+          defaultTimeEnd={moment().add(96, 'hour')}
+        />
+        <AddEvent eventList={eventList} setEventList={setEventList}/>
+      </>:<h1>loading</h1>):
+      <LogIn handleItemClick = {handleItemClick}/>}
+      <button onClick={(e) => handleItemClick(e, 'sign-out')}>sign-out</button>
+      <button onClick={(e) => handleItemClick(e, 'list')}>list</button>
+    </>
   );
 }
 
